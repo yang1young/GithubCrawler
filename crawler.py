59b33c2180364c62258e92002b98005e9eed7572
yang <1031545274@qq.com>
2017-05-24 16:25:58 +0800

    get information from API

--- crawler.py
+++ crawler.py
@@ -1,22 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import urllib
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import json, requests
 
-token = 'a632afd558411ee249ad7d80f083f44e2a47c5fb'
-#url = "https://api.github.com/repositories?since=" + since+"?q=language:Java&sort=stars&order=desc"
-page = '3'
-url = "https://api.github.com/search/repositories?q=language:Java&per_page=100&page="+page+"&access_token="+token
-res = json.loads(requests.get(url).content)
-items = dict(res).get("items")
+TOKEN = '5e507923a11636aa1c7d99b3118e7fc41600a26d'
+MAX_PAGE = 1
+ITEM_PER_PAGE = 3
 
-for item in items:
+
+def mytest():
+    page = '3'
+    url = "https://api.github.com/search/repositories?q=language:Java&per_page=10&page="+page#+"&access_token="+TOKEN
+    payload = {"Accept":"application/vnd.github.mercy-preview+json","Authorization": "token "+TOKEN}
+    #url = 'https://api.github.com/repos/libgdx/libgdx'
+    a = requests.get(url,headers=payload)
+    res = json.loads(a.content)
+    dict_res = dict(res)
+    print res
+
+    #     if(dict_item.get("name")=='android-classyshark'):
+    #           for i, j in dict(item).iteritems():
+    #               print str(i)+':'+str(j)
+
+
+def get_information(item):
 
     dict_item = dict(item)
-    if(dict_item.get("name")=='android-classyshark'):
-        for i, j in dict(item).iteritems():
-            print str(i)+':'+str(j)
+    project_name = dict_item.get("name")
+    url =  dict_item.get("url")
+    git_url = dict_item.get("git_url")
+    payload = {"Accept":"application/vnd.github.mercy-preview+json","Authorization": "token "+TOKEN}
+    res = dict(json.loads(requests.get(url,headers=payload).content))
+    topics = res.get('topics')
+    print project_name,git_url,topics
 
-#print json.loads(requests.get("https://api.github.com/rate_limit?access_token="+token).content)
+
+def crawl_url():
+    id = 0
+    for i in range(MAX_PAGE):
+        id +=1
+        url = "https://api.github.com/search/repositories?q=language:Java&per_page="+ str(ITEM_PER_PAGE)+"&page="+ str(i) + "&access_token=" + TOKEN
+        res = json.loads(requests.get(url).content)
+        items = dict(res).get("items")
+        for item in items:
+            get_information(item)
+
+
+
+
+
+crawl_url()
+#mytest()
